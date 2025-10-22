9562845f52d6a7f23d8651dd9fb0d4c808e56a37
JonasMahlburg <darrylmanga@yahoo.com>
2025-10-22 08:19:52 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jonas Mahlburg on 22.10.25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
